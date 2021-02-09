a6780c261c7a05ee8faabd9aa8b9b3e5c958f8f4
Cadenzaputra <cadenzaputra11@gmail.com>
2021-02-09 14:26:56 +0700

    buat content login

--- script.js
+++
@@ -1,7 +0,0 @@
-function klik() {
-    let password = document.getElementById('password').value;
-    if (password == atob('YXN1')) {//asu
-        return window.location.replace('login.html');
-    }
-    return alert("password salah");
-}
